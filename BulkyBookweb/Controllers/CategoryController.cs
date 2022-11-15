b102444834edf294dbf4818c9cce60a863136506
phanice owino <owinophanice54@gmail.com>
2022-11-15 17:12:37 +0300

    made cahenges on configuration

--- BulkyBookweb/Controllers/CategoryController.cs
+++ BulkyBookweb/Controllers/CategoryController.cs
@@ -18,5 +18,12 @@ namespace BulkyBookweb.Controllers
             IEnumerable<Category> objCategoryList =_db.Categories;
             return View(objCategoryList);
         }
+
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
     }
+
 }
