dabbb75b2aa9b281d4748dd9ca7c2390f16d7287
phanice owino <owinophanice54@gmail.com>
2022-11-15 15:16:54 +0300

    configured application db context inside cattegory controller to enable it work with the database

--- BulkyBookweb/Controllers/CategoryController.cs
+++ BulkyBookweb/Controllers/CategoryController.cs
@@ -1,10 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyBookweb.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBookweb.Controllers
 {
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+         public IActionResult Index()
         {
             return View();
         }
