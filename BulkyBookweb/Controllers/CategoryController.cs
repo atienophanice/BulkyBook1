e6502e4392eaca4450270956782babc173594109
phanice owino <owinophanice54@gmail.com>
2022-11-16 13:54:57 +0300

    added a return view incase the validation fails

--- BulkyBookweb/Controllers/CategoryController.cs
+++ BulkyBookweb/Controllers/CategoryController.cs
@@ -32,11 +32,17 @@ namespace BulkyBookweb.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
         {
-            if(ModelState.IsValid)
-            _db.Categories.Add(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj)
         }
+
+
     }
 
 }
