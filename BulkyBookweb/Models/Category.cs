ce346069a79e9bbc1793dfaa274a4367365c90ee
phanice owino <owinophanice54@gmail.com>
2022-11-16 15:21:07 +0300

    configures the dispaly order tpo have space in between

--- BulkyBookweb/Models/Category.cs
+++ BulkyBookweb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyBookweb.Models
 {
@@ -9,6 +10,7 @@ namespace BulkyBookweb.Models
         [Required]
         public string Name { get; set; }
 
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
 
         public DateTime  CreatedDateTime { get; set; }
